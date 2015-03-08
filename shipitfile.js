721271e677a3be7011ff1bf6a30d77c800818bc9
Chris Hegre <christopher.hegre@bigcommerce.com>
2015-03-08 16:33:03 -0700

    Automated deployment workflow

---
+++ shipitfile.js
@@ -0,0 +1,30 @@
+
+
+module.exports = function (shipit) {
+    require('shipit-deploy')(shipit);
+
+    shipit.initConfig({
+        default: {
+            workspace: '/tmp/immodispo-api',
+            deployTo: '/opt/immodispo-api',
+            repositoryUrl: 'https://github.com/hegrec/immodispo-api.git',
+            ignores: ['.git', 'node_modules'],
+            keepReleases: 3
+        },
+        staging: {
+            servers: 'caketoast.com'
+        },
+        production: {
+            servers: 'mealtrap.com'
+        }
+    });
+
+    shipit.on('published', function() {
+        shipit.remote('cd /opt/immodispo-api/current && npm install').then(function(res) {
+            shipit.log(res);
+            shipit.remote('cp /home/deploy/env/immodispo-api/env.js /opt/immodispo-api/current').then(function(res) {
+                shipit.log(res);
+            });
+        });
+    })
+};
